82dcc873898e7812b50c59fd00a349ad9b6bfcf5
Cayo322 <112021884+Cayo322@users.noreply.github.com>
2022-09-13 21:23:43 -0500

    laboratorio 03

---
+++ JavaS/Tipos de datos.js
@@ -0,0 +1,14 @@
+/* Ejemplos de
+tipos de datos
+en JavaScript */ 
+//Tipo de dato string
+var nombre = "Carlos"; 
+console.log(nombre);
+//Tipo de dato numerico var numero = 1000; console.log(numero);
+//Tipo de dato object
+var objecto = { 
+    telefono: "55443322",
+    nombre : "Juan", 
+    apellido: "Perez",
+}
+console.log(objecto);
---
+++ JavaS/ejercicio1.js
@@ -0,0 +1,12 @@
+var nombre = 'Hola';
+console.log(nombre);
+
+var numero = 1000;
+console.log( typeof numero + numero);
+
+var objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+    telefono: 55443322
+}
+console.log(objeto);
